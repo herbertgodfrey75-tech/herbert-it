bb0fc83ee3aee567b62580d8478d5b4a8d62a223
herbertgodfrey75-tech <herbertgodfrey75@gmail.com>
2026-06-30 14:16:30 +0100

    update URL

--- farm-frontend/src/App.jsx
+++ farm-frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
-const API_URL = " https://herbert-it.onrender.com";
+const API_URL = "https://herbert-it.onrender.com";
 
 
 function App() {
