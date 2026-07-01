8921213779c65832bda601b46b811f7c35e8fc66
herbertgodfrey75-tech <herbertgodfrey75@gmail.com>
2026-07-01 13:21:22 +0100

    Updated Backend CORS

--- farm-backend/main.py
+++ farm-backend/main.py
@@ -58,27 +58,21 @@ async def startup_event():
 
 
 
-# CORS
+# CORS SETTINGS
 
 app.add_middleware(
-
     CORSMiddleware,
 
     allow_origins=[
-
         "https://herbert-it-1.onrender.com",
-
-        "http://localhost:5173"
-
+        "http://localhost:5173",
     ],
-
 
     allow_credentials=True,
 
     allow_methods=["*"],
 
     allow_headers=["*"],
-
 )
 
 
